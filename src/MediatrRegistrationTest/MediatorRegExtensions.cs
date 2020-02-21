f361dd113d663fbe2f2805aaf063fc3e5ad8862a
Sergey Golodyuk <sergey.golodyuk@altexsoft.com>
2020-02-21 18:41:15 +0200

    Move extension to separate file

---
+++ src/MediatrRegistrationTest/MediatorRegExtensions.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System.Threading.Tasks;
+using System.Threading;
+using FluentValidation;
+
+namespace MediatrRegistrationTest
+{
+    public static class MediatorRegExtensions
+    {
+        public static ScopedRegistrator<TRequest, TResponse> ForScoped<TRequest, TResponse>(this IServiceCollection services) where TRequest : IRequest<TResponse>
+        {
+            return new ScopedRegistrator<TRequest, TResponse>(services);
+        }
+
+        public class ScopedRegistrator<TRequest, TResponse> where TRequest : IRequest<TResponse>
+        {
+            private readonly IServiceCollection _services;
+
+            public ScopedRegistrator(IServiceCollection services)
+            {
+                _services = services;
+            }
+
+            public ScopedRegistrator<TRequest, TResponse> WithValidation<TValidator>() where TValidator : AbstractValidator<TRequest>
+            {
+                _services.AddScoped<TValidator>();
+                _services.AddScoped<IPipelineBehavior<TRequest, TResponse>>(p => new ValidationBehavior<TRequest, TResponse>(p.GetService<TValidator>()));
+
+                return this;
+            }
+
+            public ScopedRegistrator<TRequest, TResponse> WithHandler<THandler>() where THandler : class, IRequestHandler<TRequest, TResponse>
+            {
+                _services.AddScoped<IRequestHandler<TRequest, TResponse>, THandler>();
+
+                return this;
+            }
+        }
+
+        public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        {
+            private readonly AbstractValidator<TRequest> _validator;
+
+            public ValidationBehavior(AbstractValidator<TRequest> validator)
+            {
+                _validator = validator;
+            }
+
+            public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+            {
+                var validationResult = await _validator
+                    .ValidateAsync(request, cancellationToken)
+                    .ConfigureAwait(false);
+
+                if (!validationResult.IsValid)
+                {
+                    return default(TResponse);
+                }
+
+                return await next();
+            }
+        }
+    }
+}
--- src/MediatrRegistrationTest/Program.cs
+++ src/MediatrRegistrationTest/Program.cs
@@ -11,8 +11,7 @@ namespace MediatrRegistrationTest
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
+            // registration start
             var collection = new ServiceCollection();
 
             collection.AddScoped<IMediator, Mediator>();
@@ -24,6 +23,10 @@ namespace MediatrRegistrationTest
 
             var provider = collection.BuildServiceProvider();
 
+            // registration end
+
+            // usage start
+
             var mediator = provider.GetService<IMediator>();
 
             var result1 = await mediator.Send(new SomeRequest());
@@ -31,7 +34,19 @@ namespace MediatrRegistrationTest
 
             var result2 = await mediator.Send(new SomeRequest { In = "something" });
             Console.WriteLine("Should be valid and return result: " + result2.Out);
+
+            // usage end
         }
+    }
+
+    public class SomeResponse
+    {
+        public string Out { get; set; }
+    }
+
+    public class SomeRequest : IRequest<SomeResponse>
+    {
+        public string In { get; set; }
     }
 
     public class SomeValidator : AbstractValidator<SomeRequest>
@@ -47,73 +62,6 @@ namespace MediatrRegistrationTest
         public Task<SomeResponse> Handle(SomeRequest request, CancellationToken cancellationToken)
         {
             return Task.FromResult(new SomeResponse { Out = request.In + "+result" });
-        }
-    }
-
-    public class SomeResponse
-    {
-        public string Out { get; set; }
-    }
-
-    public class SomeRequest : IRequest<SomeResponse>
-    {
-        public string In { get; set; }
-    }
-
-    public static class MediatorRegExtensions
-    {
-        public static ScopedRegistrator<TRequest, TResponse> ForScoped<TRequest, TResponse>(this IServiceCollection services) where TRequest : IRequest<TResponse>
-        {
-            return new ScopedRegistrator<TRequest, TResponse>(services);
-        }
-
-        public class ScopedRegistrator<TRequest, TResponse> where TRequest : IRequest<TResponse>
-        {
-            private readonly IServiceCollection _services;
-
-            public ScopedRegistrator(IServiceCollection services)
-            {
-                _services = services;
-            }
-
-            public ScopedRegistrator<TRequest, TResponse> WithValidation<TValidator>() where TValidator : AbstractValidator<TRequest>
-            {
-                _services.AddScoped<TValidator>();
-                _services.AddScoped<IPipelineBehavior<TRequest, TResponse>>(p => new ValidationBehavior<TRequest, TResponse>(p.GetService<TValidator>()));
-
-                return this;
-            }
-
-            public ScopedRegistrator<TRequest, TResponse> WithHandler<THandler>() where THandler : class, IRequestHandler<TRequest, TResponse>
-            {
-                _services.AddScoped<IRequestHandler<TRequest, TResponse>, THandler>();
-
-                return this;
-            }
-        }
-
-        public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-        {
-            private readonly AbstractValidator<TRequest> _validator;
-
-            public ValidationBehavior(AbstractValidator<TRequest> validator)
-            {
-                _validator = validator;
-            }
-
-            public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
-            {
-                var validationResult = await _validator
-                    .ValidateAsync(request, cancellationToken)
-                    .ConfigureAwait(false);
-
-                if (!validationResult.IsValid)
-                {
-                    return default(TResponse);
-                }
-
-                return await next();
-            }
         }
     }
 }
